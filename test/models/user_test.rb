fefddfba58dbf65d68f032cb37efdfe2ec64f08e
tanya.ru21 <tanya.ru21@gmail.com>
2017-04-05 17:22:30 -0400

    chapter 6, adding uniqueness

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -48,5 +48,11 @@ test "email validation should accept valid addresses" do
     end
   end
 
+test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 
 end
