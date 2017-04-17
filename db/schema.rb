d52ff0cd44d8d59545afc947a59eee74e60b014d
tanya.ru21 <tanya.ru21@gmail.com>
2017-04-17 15:19:13 -0400

    added a migration for remember digest tokens

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170405213701) do
+ActiveRecord::Schema.define(version: 20170417185637) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170405213701) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
 end
