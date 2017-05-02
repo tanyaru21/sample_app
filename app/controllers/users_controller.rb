32ed4ac18a9ae86c360053cd1f5cdac490812a64
tanya.ru21 <tanya.ru21@gmail.com>
2017-05-02 18:19:00 -0400

    more changes chapter 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,8 +23,18 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      #handle a successfull update
+    else
+      render 'edit'
+    end
+  end
+
   def user_params
   	params.require(:user).permit(:name, :email, :password, 
   		:password_confirmation)
   end
+
 end
