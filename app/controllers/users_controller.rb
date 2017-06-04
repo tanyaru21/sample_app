1443b4bfa63750dcc5f9b98507d557cab9c400b1
tanyaru21 <tanya.ru21@gmail.com>
2017-06-04 11:23:36 -0400

    microposts tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,16 +56,6 @@ private
 
   #before filters
 
-  #confirms a logged in user
-
-  def logged_in_user
-    unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_path
-    end  
-  end
-
   #confirms the correct user 
 
   def correct_user
--- db/seeds.rb
+++ db/seeds.rb
@@ -27,7 +27,6 @@ User.create!(name: "Example User1",
 
 users = User.order(:created_at).take(6)
 50.times do
-	content = Faker::Lorem.sentence(5)
-	users.each { |user| user.microposts.create!(content:content) }
-end
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
 end
